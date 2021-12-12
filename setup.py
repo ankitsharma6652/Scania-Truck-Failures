3d213d85edc9d169121d2290e26c3c1cff6ab12f
Devashri Chaudhari <DevashriChaudhari@gmail.com>
2021-12-12 09:31:52 +0530

    project structure added

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     description="To predict whether a failure of a Scania Truck component is related to the air pressure system (APS) or not.",
     author="Devashri Chaudhari and Ankit Sharma", 
     packages=find_packages(),
+    python_requires=">=3.7",
     license="MIT"
 )
