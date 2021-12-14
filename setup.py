d7fe5269e07a04478dfae6cd91f2d30d1f15bed4
Devashri Chaudhari <DevashriChaudhari@gmail.com>
2021-12-14 16:55:42 +0530

    setup updated

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="src",
     version="0.0.1",
     description="To predict whether a failure of a Scania Truck component is related to the air pressure system (APS) or not.",
-    url="https://github.com/ankitsharma6652/Scania-Truck-Failures",
+    url="git@github.com:ankitsharma6652/Scania-Truck-Failures.git",
     author="Devashri Chaudhari and Ankit Sharma", 
     packages=["src"],
     python_requires=">=3.7",
