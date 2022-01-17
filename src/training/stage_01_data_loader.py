1af7426ce2522d70c2dbeb2109f81f48a8da43ae
Devashri Chaudhari <DevashriChaudhari@gmail.com>
2022-01-17 19:09:01 +0530

    Model Training added

--- src/training/stage_01_data_loader.py
+++ src/training/stage_01_data_loader.py
@@ -6,7 +6,7 @@ import pandas as pd
 # import s3fs
 
 logging_str = "[%(asctime)s: %(levelname)s %(module)s]: %(message)s"
-log_dir = "logs"
+log_dir = "../../logs"
 
 os.makedirs(log_dir, exist_ok=True)
 logging.basicConfig(filename=os.path.join(log_dir, "running_logs.log"), level=logging.INFO, format=logging_str, filemode='a')
