17a74253fbea2e805b2ed931e2ddbf7f6d2d230e
ankitsharma6652 <ankitcoolji@gmail.com>
2022-03-06 07:29:30 +0530

    Updated

--- app.py
+++ app.py
@@ -368,9 +368,10 @@ def scheduler(email_address):
 #     return render_template("scheduler_manager.html")
 port = int(os.getenv("PORT",5000))
 if __name__ == "__main__":
-    host = '0.0.0.0'
-    # port = 5000
-    # print(email_address)
-    httpd = simple_server.make_server(host, port, app)
-    # print("Serving on %s %d" % (host, port))
-    httpd.serve_forever()
+    # host = '0.0.0.0'
+    # # port = 5000
+    # # print(email_address)
+    # httpd = simple_server.make_server(host, port, app)
+    # # print("Serving on %s %d" % (host, port))
+    # httpd.serve_forever()
+    app.run()
